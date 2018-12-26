c6af5d6f240f95962a43d6a097fffde850746923
jose2007kj <jose2007kj@gmail.com>
2018-12-26 16:22:35 +0530

    made some changes to fit for atdab

--- android/src/main/java/com/goldenowl/twittersignin/TwitterSigninModule.java
+++ android/src/main/java/com/goldenowl/twittersignin/TwitterSigninModule.java
@@ -3,7 +3,8 @@ package com.goldenowl.twittersignin;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
-
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -15,6 +16,7 @@ import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.SessionManager;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterConfig;
@@ -22,6 +24,7 @@ import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.models.User;
 
 import java.util.Map;
 import java.util.Set;
@@ -30,11 +33,14 @@ public class TwitterSigninModule extends ReactContextBaseJavaModule implements A
 
     public static String TAG = "RNTwitterSignIn";
     private final ReactApplicationContext reactContext;
+    private SharedPreferences prefs;
+    // public final TwitterSession session;
     TwitterAuthClient twitterAuthClient;
 
     public TwitterSigninModule(ReactApplicationContext reactContext) {
         super(reactContext);
         reactContext.addActivityEventListener(this);
+        prefs=PreferenceManager.getDefaultSharedPreferences(reactContext);
         this.reactContext = reactContext;
     }
 
@@ -61,30 +67,56 @@ public class TwitterSigninModule extends ReactContextBaseJavaModule implements A
         twitterAuthClient.authorize(getCurrentActivity(), new com.twitter.sdk.android.core.Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                final TwitterSession session = result.data;
+                TwitterSession session = result.data;
                 TwitterAuthToken twitterAuthToken = session.getAuthToken();
                 final WritableMap map = Arguments.createMap();
                 map.putString("authToken", twitterAuthToken.token);
                 map.putString("authTokenSecret", twitterAuthToken.secret);
                 map.putString("name", session.getUserName());
                 map.putString("userID", Long.toString(session.getUserId()));
+                try{
+                    prefs.edit().putString("userId",Long.toString(session.getUserId())).commit();
+                }
+                catch(Exception e){
+                    map.putString("pref", e.toString());
+                            promise.reject("COULD_NOT_FETCH", map.toString());
+                }
+                
                 map.putString("userName", session.getUserName());
-                twitterAuthClient.requestEmail(session, new com.twitter.sdk.android.core.Callback<String>() {
+                // twitterAuthClient.requestEmail(session, new com.twitter.sdk.android.core.Callback<String>() {
+                //     @Override
+                //     public void success(Result<String> result) {
+                //         map.putString("email", result.data);
+                //         promise.resolve(map);
+                //     }
+
+                //     @Override
+                //     public void failure(TwitterException exception) {
+                //         map.putString("email", "COULD_NOT_FETCH");
+                //         promise.reject(
+                //                 "COULD_NOT_FETCH",
+                //                 map.toString(),
+                //                 new Exception("Failed to obtain email", exception));
+                //     }
+                // });//commened by jose and below code added by jose
+                TwitterCore.getInstance().getApiClient(session).getAccountService().verifyCredentials(false,true,true).enqueue(new Callback<User>() {
                     @Override
-                    public void success(Result<String> result) {
-                        map.putString("email", result.data);
+                    public void success(Result<User> userResult) {
+                        try {
+                            map.putString("email", userResult.data.email);
+                            map.putString("name", userResult.data.screenName);
+                            map.putString("avatar", userResult.data.profileImageUrlHttps);
+                            promise.resolve(map);
+                        } catch (Exception e) {
+                            map.putString("email", "COULD_NOT_FETCH");
+                            promise.reject("COULD_NOT_FETCH", map.toString());
+                        }
+                    }
+                    @Override
+                    public void failure(TwitterException e) {
                         promise.resolve(map);
                     }
-
-                    @Override
-                    public void failure(TwitterException exception) {
-                        map.putString("email", "COULD_NOT_FETCH");
-                        promise.reject(
-                                "COULD_NOT_FETCH",
-                                map.toString(),
-                                new Exception("Failed to obtain email", exception));
-                    }
-                });
+                });//jose code ends here
             }
 
             @Override
@@ -109,7 +141,37 @@ public class TwitterSigninModule extends ReactContextBaseJavaModule implements A
 
         sessionManager
                 .clearActiveSession();
+        final WritableMap map = Arguments.createMap();
+        try{
+            prefs.edit().putString("userId","").commit();
+        }catch(Exception e){
+            
+        }
     }
+    //added to get current session jose2007kj
+    @ReactMethod
+    public void getUserId(final Promise promise) {
+        final WritableMap map = Arguments.createMap();
+        try{
+            map.putString("userId", prefs.getString("userId",""));
+            promise.resolve(map);
+        }catch(Exception e){
+            
+            map.putString("userId", e.toString());
+            promise.reject("COULD_NOT_FETCH", map.toString());
+        }
+    }
+    // @ReactMethod
+    // public void clearUserId(final Promise promise) {
+    //     final WritableMap map = Arguments.createMap();
+    //     try{
+    //         map.putString("userId","");
+    //     }catch(Exception e){
+            
+    //         map.putString("userId", e.toString());
+    //         promise.reject("COULD_NOT_FETCH", map.toString());
+    //     }
+    // }
 
     @Override
     public void onNewIntent(Intent intent) {
@@ -122,3 +184,128 @@ public class TwitterSigninModule extends ReactContextBaseJavaModule implements A
         }
     }
 }
+
+// package com.goldenowl.twittersignin;
+
+// import android.app.Activity;
+// import android.content.Intent;
+// import android.util.Log;
+
+// import com.facebook.react.bridge.ActivityEventListener;
+// import com.facebook.react.bridge.Arguments;
+// import com.facebook.react.bridge.Promise;
+// import com.facebook.react.bridge.ReactApplicationContext;
+// import com.facebook.react.bridge.ReactContextBaseJavaModule;
+// import com.facebook.react.bridge.ReactMethod;
+// import com.facebook.react.bridge.WritableMap;
+// import com.twitter.sdk.android.core.DefaultLogger;
+// import com.twitter.sdk.android.core.Result;
+// import com.twitter.sdk.android.core.SessionManager;
+// import com.twitter.sdk.android.core.Twitter;
+// import com.twitter.sdk.android.core.TwitterAuthConfig;
+// import com.twitter.sdk.android.core.TwitterAuthToken;
+// import com.twitter.sdk.android.core.TwitterConfig;
+// import com.twitter.sdk.android.core.TwitterCore;
+// import com.twitter.sdk.android.core.TwitterException;
+// import com.twitter.sdk.android.core.TwitterSession;
+// import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+
+// import java.util.Map;
+// import java.util.Set;
+
+// public class TwitterSigninModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+
+//     public static String TAG = "RNTwitterSignIn";
+//     private final ReactApplicationContext reactContext;
+//     TwitterAuthClient twitterAuthClient;
+
+//     public TwitterSigninModule(ReactApplicationContext reactContext) {
+//         super(reactContext);
+//         reactContext.addActivityEventListener(this);
+//         this.reactContext = reactContext;
+//     }
+
+//     @Override
+//     public String getName() {
+//         return TAG;
+//     }
+
+//     @ReactMethod
+//     public void init(String consumerKey, String consumerSecret, Promise promise) {
+//         TwitterConfig config = new TwitterConfig.Builder(this.reactContext)
+//                 .logger(new DefaultLogger(Log.DEBUG))
+//                 .twitterAuthConfig(new TwitterAuthConfig(consumerKey, consumerSecret))
+//                 .debug(true)
+//                 .build();
+//         Twitter.initialize(config);
+//         WritableMap map = Arguments.createMap();
+//         promise.resolve(map);
+//     }
+
+//     @ReactMethod
+//     public void logIn(final Promise promise) {
+//         twitterAuthClient = new TwitterAuthClient();
+//         twitterAuthClient.authorize(getCurrentActivity(), new com.twitter.sdk.android.core.Callback<TwitterSession>() {
+//             @Override
+//             public void success(Result<TwitterSession> result) {
+//                 final TwitterSession session = result.data;
+//                 TwitterAuthToken twitterAuthToken = session.getAuthToken();
+//                 final WritableMap map = Arguments.createMap();
+//                 map.putString("authToken", twitterAuthToken.token);
+//                 map.putString("authTokenSecret", twitterAuthToken.secret);
+//                 map.putString("name", session.getUserName());
+//                 map.putString("userID", Long.toString(session.getUserId()));
+//                 map.putString("userName", session.getUserName());
+//                 twitterAuthClient.requestEmail(session, new com.twitter.sdk.android.core.Callback<String>() {
+//                     @Override
+//                     public void success(Result<String> result) {
+//                         map.putString("email", result.data);
+//                         promise.resolve(map);
+//                     }
+
+//                     @Override
+//                     public void failure(TwitterException exception) {
+//                         map.putString("email", "COULD_NOT_FETCH");
+//                         promise.reject(
+//                                 "COULD_NOT_FETCH",
+//                                 map.toString(),
+//                                 new Exception("Failed to obtain email", exception));
+//                     }
+//                 });
+//             }
+
+//             @Override
+//             public void failure(TwitterException exception) {
+//                 promise.reject("USER_CANCELLED", exception.getMessage(), exception);
+//             }
+//         });
+//     }
+
+//     @ReactMethod
+//     public void logOut() {
+
+//         TwitterCore instance = TwitterCore.getInstance();
+//         SessionManager<TwitterSession> sessionManager = instance.getSessionManager();
+//         Map<Long, TwitterSession> sessions = sessionManager.getSessionMap();
+//         System.out.println("TWITTER SEESIONS " + +sessions.size());
+//         Set<Long> sessids = sessions.keySet();
+//         for (Long sessid : sessids) {
+//             System.out.println("TWITTER SESSION CLEARING " + sessid);
+//             instance.getSessionManager().clearSession(sessid);
+//         }
+
+//         sessionManager
+//                 .clearActiveSession();
+//     }
+
+//     @Override
+//     public void onNewIntent(Intent intent) {
+//     }
+
+//     @Override
+//     public void onActivityResult(Activity currentActivity, int requestCode, int resultCode, Intent data) {
+//         if (twitterAuthClient != null && twitterAuthClient.getRequestCode() == requestCode) {
+//             twitterAuthClient.onActivityResult(requestCode, resultCode, data);
+//         }
+//     }
+// }
